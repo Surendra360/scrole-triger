48611b3c4e27e90824a0999faa1d9ae960b15846
Surendra Kumar <88983549+Surendra360@users.noreply.github.com>
2024-02-03 00:08:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ gsap.from(".page2 .box1",{
     duration:1,
     scale:0,
     scrollTrigger:{
-        trigger:
+        trigger: ".page2 .box1",
+        scroller:"body",
+        markers:true,
+        start: "top 50%",
+        end:"top 30%",
+        scrub:5
+       // scrub:true,  //true se koi smothness nahi aaegi "true" ki jagah 1,2...5 me se koi ek number dene se smothness milti h acording to number
     }
     
 })
