3931fd57fafcb786000899d21334aefed3b23ddc
Surendra Kumar <88983549+Surendra360@users.noreply.github.com>
2024-02-03 00:00:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.from(".page1 .box1",{
+    rotate:360,
+    duration:1,
+    delay:1,
+    scale:0
+})
+gsap.from(".page2 .box1",{
+    rotate:360,
+    duration:1,
+    scale:0,
+    scrollTrigger:{
+        trigger:
+    }
+    
+})
